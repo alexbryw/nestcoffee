74e26152efdb6a2b3d1d72102aaa5a9ab8545bd3
alexbryw <alexbryw@gmail.com>
2024-12-19 21:40:22 +0100

    TypeORM autogenerate SchemaSync migration, added column 'description'

--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -1,5 +1,8 @@
 import { DataSource } from 'typeorm';
-import { CoffeeRefactor1734637944782 } from 'src/migrations/1734637944782-CoffeeRefactor';
+//import { CoffeeRefactor1734637944782 } from 'src/migrations/1734637944782-CoffeeRefactor';
+import { Coffee } from 'src/coffees/entities/coffees.entity';
+import { Flavor } from 'src/coffees/entities/flavor.entity/flavor.entity';
+import { SchemaSync1734640332223 } from 'src/migrations/1734640332223-SchemaSync';
 
 export default new DataSource({
   type: 'postgres',
@@ -8,6 +11,7 @@ export default new DataSource({
   username: 'postgres',
   password: 'pass123',
   database: 'postgres',
-  entities: [],
-  migrations: [CoffeeRefactor1734637944782],
+  entities: [Coffee, Flavor],
+  migrations: [SchemaSync1734640332223],
+  //migrations: [CoffeeRefactor1734637944782, SchemaSync1734640332223],
 });
