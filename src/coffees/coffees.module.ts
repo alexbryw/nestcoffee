8a14d6029df75ebbf430d50e0bf14c12341359fa
alexbryw <alexbryw@gmail.com>
2024-12-20 17:15:36 +0100

    undo value provider

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,16 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffees.entity';
 import { Flavor } from './entities/flavor.entity/flavor.entity';
 import { Event } from 'src/events/entities/event.entity/event.entity';
-export class MockCoffeesService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [
-    {
-      provide: CoffeesService,
-      useValue: new MockCoffeesService(),
-    },
-  ],
+  providers: [CoffeesService],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
