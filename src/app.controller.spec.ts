d2c1bcde922c776ff1260ced483784bfb09a6bb8
alexbryw <alexbryw@gmail.com>
2024-12-26 13:39:22 +0100

    update test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello Coffee!"', () => {
+      expect(appController.getHello()).toBe('Hello Coffee!');
     });
   });
 });
